bdb441c13621db9f8b0a743d4ba635604d36e461
Subham Mani <manisubham09@gmail.com>
2025-01-05 23:49:21 +0530

    add wildcard

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -22,5 +22,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/dashboard", "/whiteboard"],
+  matcher: ["/", "/dashboard", "/whiteboard/:path*"],
 };
