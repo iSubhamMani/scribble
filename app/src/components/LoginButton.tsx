d76537d1c286c2ecde21c5413b14fe0004d4ef9a
Subham Mani <manisubham09@gmail.com>
2025-01-08 14:34:50 +0530

    update LoginButton to change label and add icon for improved user experience

--- app/src/components/LoginButton.tsx
+++ app/src/components/LoginButton.tsx
@@ -3,6 +3,7 @@
 import { signIn } from "next-auth/react";
 import ShinyButton from "./ui/shiny-button";
 import Image from "next/image";
+import { MoveRight } from "lucide-react";
 
 const LoginButton = () => {
   return (
@@ -11,9 +12,10 @@ const LoginButton = () => {
         onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
         className="text-black rounded-full font-bold px-8 border border-primary "
       >
-        <span className="flex items-center gap-2">
+        <span className="flex items-center gap-2 text-primary-foreground">
           <Image src={"/google.svg"} width={36} height={36} alt="Google logo" />
-          Continue with Google
+          Try Scribble
+          <MoveRight />
         </span>
       </ShinyButton>
     </div>
