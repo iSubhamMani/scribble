043f789e0ae368ad44354296e35791628638805d
Subham Mani <manisubham09@gmail.com>
2025-01-08 14:33:56 +0530

    update app title and enhance homepage layout with new background and logo

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,33 +1,41 @@
 import LoginButton from "@/components/LoginButton";
+import DotPattern from "@/components/ui/dot-pattern";
 import Meteors from "@/components/ui/meteors";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col overflow-hidden relative ">
-      <Meteors number={10} />
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+    <div className="flex min-h-screen flex-col overflow-hidden relative bg-background bg-[radial-gradient(ellipse_90%_90%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <Meteors number={5} />
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(250px_circle_at_center,white,transparent)]"
+        )}
+      />
+      <header className="px-6 lg:px-10 mt-8 flex items-center justify-center">
         <Link className="flex items-center justify-center" href="#">
-          <div className="flex items-center">
-            <span className="font-bold text-lg md:text-2xl text-primary">
-              Board Mates
+          <div className="flex items-center gap-2">
+            <Image src={"/scribble.png"} width={32} height={32} alt="logo" />
+            <span className="patrick-hand font-bold text-2xl md:text-3xl text-primary-foreground">
+              Scribble
             </span>
           </div>
         </Link>
       </header>
-      <main className="flex-1 w-full max-w-6xl mx-auto">
+      <main className="flex-1 max-w-6xl mx-auto">
         <section className="py-20 md:py-28 lg:py-32 xl:py-40">
-          <div className="container px-4 md:px-6">
+          <div className="container px-6 lg:px-10">
             <div className="flex flex-col items-center space-y-8 text-center">
               <div className="space-y-4">
-                <h1 className="mb-6 fade-pullup text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl/none xl:text-7xl/none">
-                  Sketch your team&apos;s
+                <h1 className="tracking-wide mb-6 fade-pullup text-4xl font-bold sm:text-5xl md:text-6xl/none xl:text-7xl/none">
+                  Scribble Your Team&apos;s
                   <br />
                   Ideas Together
                 </h1>
-                <p className="fade-pullup-delayed-1 font-medium text-primary md:text-xl">
-                  A collaborative whiteboard for your team to bring ideas to
-                  life.
+                <p className="fade-pullup-delayed-1 font-medium text-accent md:text-xl">
+                  Because Brilliant Ideas Don&apos;t Stay in Boxes
                 </p>
               </div>
               <LoginButton />
