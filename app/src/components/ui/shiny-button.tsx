d3dd65e88d054e22c38e004127068e37c799a0f9
Subham Mani <manisubham09@gmail.com>
2025-01-08 14:32:58 +0530

    modify color

--- app/src/components/ui/shiny-button.tsx
+++ app/src/components/ui/shiny-button.tsx
@@ -42,25 +42,24 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
         {...animationProps}
         {...props}
         className={cn(
-          "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
+          "relative rounded-lg px-6 py-2 font-medium transition-shadow duration-300 ease-in-out bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/20%)_100%,transparent_100%)] hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
           className
         )}
       >
         <span
-          className="relative block size-full text-sm md:text-base tracking-wide dark:font-light"
+          className="text-primary-foreground relative block size-full text-sm md:text-base tracking-wide font-medium"
           style={{
             maskImage:
-              "linear-gradient(-75deg,hsl(var(--primary)) calc(var(--x) + 20%),transparent calc(var(--x) + 30%),hsl(var(--primary)) calc(var(--x) + 100%))",
+              "linear-gradient(-75deg,hsl(var(--primary)) calc(var(--x) + 5%),transparent calc(var(--x) + 10%),hsl(var(--primary)) calc(var(--x) + 100%))",
           }}
         >
           {children}
         </span>
         <span
           style={{
-            mask: "linear-gradient(rgb(0,0,0), rgb(0,0,0)) content-box,linear-gradient(rgb(0,0,0), rgb(0,0,0))",
-            maskComposite: "exclude",
+            mask: "linear-gradient(rgb(255,255,255), rgb(255,255,255)) content-box,linear-gradient(rgb(255,255,255), rgb(255,255,255))",
           }}
-          className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,hsl(var(--primary)/10%)_calc(var(--x)+20%),hsl(var(--primary)/50%)_calc(var(--x)+25%),hsl(var(--primary)/10%)_calc(var(--x)+100%))] p-px"
+          className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,hsl(var(--primary)/10%)_calc(var(--x)+5%),hsl(var(--primary)/10%)_calc(var(--x)+5%),hsl(var(--primary)/15%)_calc(var(--x)+5%))] p-px"
         ></span>
       </motion.button>
     );
