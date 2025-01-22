545121f4dc997a4914ba3cfe268a6d4ddd779cb0
Subham Mani <manisubham09@gmail.com>
2025-01-22 17:57:09 +0530

    enhance MainLayout with a radial gradient background for improved aesthetics

--- app/src/app/(main)/layout.tsx
+++ app/src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex bg-background min-h-[100dvh]">
+    <div className="flex bg-background min-h-[100dvh] bg-[radial-gradient(ellipse_60%_60%_at_10%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]">
       <Navbar />
       {children}
     </div>
